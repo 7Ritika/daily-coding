99081bd8f14e6bd00b24a43754300c1d33a17118
Ritika <ritika7@ualberta.ca>
2026-02-10 14:50:06 -0500

    Create LC001_TwoSum.cpp

---
+++ leetcode/LC001_TwoSum.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int,int> m;
+    for (int i = 0; i < nums.size(); i++) {
+        int diff = target - nums[i];
+        if (m.count(diff)) return {m[diff], i};
+        m[nums[i]] = i;
+    }
+    return {};
+}
